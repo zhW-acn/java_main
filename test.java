994bb01bbcf27bc7ab50dc7d10ecce341d38bb12
zhW-acn <9591382+zi-hao-wangmeakua@user.noreply.gitee.com>
2023-09-04 15:38:07 +0800

    9.4

---
+++ test.java
@@ -0,0 +1,13 @@
+/**
+* @Description: TODO
+* @author: acn
+* @date: 2023/09/04/15:11
+*/
+public class test
+{
+    public static void main(String[] args) {
+        String s = "123";
+        String s1 = "123";
+        System.out.println(s == s1);
+    }
+}
