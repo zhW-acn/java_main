4de381eb277616397816daea9584a45cb462a136
zhW-acn <132061556+zhW-acn@users.noreply.github.com>
2023-09-26 22:59:18 +0800

    update

--- months/month_1/week_3/exercise/ExcelReadWrite/Write.java
+++ months/month_1/week_3/exercise/ExcelReadWrite/Write.java
@@ -1,6 +1,9 @@
 package months.month_1.week_3.exercise.ExcelReadWrite;
 
-//import org.apache.poi.xssf.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -10,43 +13,43 @@ import java.util.ArrayList;
  * @author: acn
  * @date: 2023/09/20/10:53
  */
-//public class Write {
-//    public static void write(Problem p) throws IOException {
-//        ArrayList<Problem> problems = Read.read();
-//        problems.add(p);
-//
-//        XSSFWorkbook wb = new XSSFWorkbook();
-//        XSSFSheet sheet = wb.createSheet("Sheet1");
-//        XSSFRow row;
-//        XSSFCell cell;
-//        int rowNum = 0;
-//        int i;
-//        for (Problem problem : problems) {
-//            i = 0;
-//            row = sheet.createRow(rowNum);
-//            cell = row.createCell(i++);
-//            cell.setCellValue(problem.getId());
-//            cell = row.createCell(i++);
-//            cell.setCellValue(problem.getStem());
-//            cell = row.createCell(i++);
-//            cell.setCellValue(problem.getOptionA());
-//            cell = row.createCell(i++);
-//            cell.setCellValue(problem.getOptionB());
-//            cell = row.createCell(i++);
-//            cell.setCellValue(problem.getOptionC());
-//            cell = row.createCell(i++);
-//            cell.setCellValue(problem.getOptionD());
-//            cell = row.createCell(i);
-//            cell.setCellValue(problem.getAnswer());
-//            rowNum++;
-//        }
-//
-//        FileOutputStream fileOutputStream = new FileOutputStream("F:\\GitHub\\java_main\\weeks\\week_3\\exercise\\ExcelReadWrite\\题目.xlsx");
-//        wb.write(fileOutputStream);
-//        fileOutputStream.close();
-//    }
-//
-//    public static void main(String[] args) throws IOException {
-//        write(new Problem("1", "1", "1", "1", "1", "1", "1"));
-//    }
-//}
+public class Write {
+    public static void write(Problem p) throws IOException {
+        ArrayList<Problem> problems = Read.read();
+        problems.add(p);
+
+        XSSFWorkbook wb = new XSSFWorkbook();
+        XSSFSheet sheet = wb.createSheet("Sheet1");
+        XSSFRow row;
+        XSSFCell cell;
+        int rowNum = 0;
+        int i;
+        for (Problem problem : problems) {
+            i = 0;
+            row = sheet.createRow(rowNum);
+            cell = row.createCell(i++);
+            cell.setCellValue(problem.getId());
+            cell = row.createCell(i++);
+            cell.setCellValue(problem.getStem());
+            cell = row.createCell(i++);
+            cell.setCellValue(problem.getOptionA());
+            cell = row.createCell(i++);
+            cell.setCellValue(problem.getOptionB());
+            cell = row.createCell(i++);
+            cell.setCellValue(problem.getOptionC());
+            cell = row.createCell(i++);
+            cell.setCellValue(problem.getOptionD());
+            cell = row.createCell(i);
+            cell.setCellValue(problem.getAnswer());
+            rowNum++;
+        }
+
+        FileOutputStream fileOutputStream = new FileOutputStream("F:\\GitHub\\java_main\\weeks\\week_3\\exercise\\ExcelReadWrite\\题目.xlsx");
+        wb.write(fileOutputStream);
+        fileOutputStream.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        write(new Problem("1", "1", "1", "1", "1", "1", "1"));
+    }
+}
